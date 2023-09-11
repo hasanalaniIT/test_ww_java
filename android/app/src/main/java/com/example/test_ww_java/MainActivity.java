bbbbf65d24042bc187bce2c8e50f5e66ac235500
hasanalani <hasanalani@dizaynvip.com>
2023-09-11 09:36:18 +0300

    Removed un-needed commented code

--- android/app/src/main/java/com/example/test_ww_java/MainActivity.java
+++ android/app/src/main/java/com/example/test_ww_java/MainActivity.java
@@ -112,56 +112,6 @@ public class MainActivity extends FlutterActivity {
         }
         return null;
     }
-
-//    public class MFCCExtractor {
-//        public double[] processAudio(String audioPath, int sampleRate, int nMfcc) throws IOException {
-//            String fullPath = assetFilePath(getApplicationContext(), audioPath);
-//            assert fullPath != null;
-//            File audioFile = new File(fullPath); // Create a File object from the path
-//            InputStream inStream = new FileInputStream(audioFile);
-//            int bufferSize = 128;
-//            AudioDispatcher dispatcher = new AudioDispatcher(new UniversalAudioInputStream(inStream, new TarsosDSPAudioFormat(sampleRate, bufferSize, 1, true, true)), bufferSize, 1);
-//            int samplesPerFrame = 1024; // Set your desired samples per frame
-//            float lowerFilterFreq = 300; // Set your desired lower filter frequency
-//            float upperFilterFreq = 3700; // Set your desired upper filter frequency
-//
-//            MFCC mfcc = new MFCC(samplesPerFrame, sampleRate, nMfcc, nMfcc, lowerFilterFreq, upperFilterFreq);
-//
-//            final List<float[]> mfccList = new ArrayList<>();
-//
-//            dispatcher.addAudioProcessor(mfcc);
-//
-//            dispatcher.addAudioProcessor(new AudioProcessor() {
-//                @Override
-//                public void processingFinished() {
-//                    // Do nothing here
-//                }
-//
-//                @Override
-//                public boolean process(AudioEvent audioEvent) {
-//                    // Convert MFCC coefficients to an array and add them to the list
-//                    float[] mfccCoefficients = mfcc.getMFCC();
-//                    mfccList.add(mfccCoefficients);
-//                    return true;
-//                }
-//            });
-//
-//            dispatcher.run();
-//
-//            // Compute the mean of MFCC coefficients
-//            double[] mfccMean = new double[nMfcc];
-//            for (int i = 0; i < nMfcc; i++) {
-//                double sum = 0;
-//                for (float[] frame : mfccList) {
-//                    sum += frame[i];
-//                }
-//                mfccMean[i] = sum / mfccList.size();
-//            }
-//
-//            return mfccMean;
-//        }
-//
-//    }
-
+    
 }
 
